e078bd058d9f4f92d58850f7fc9ec1cccaab0ee4
2kreps <rajuottkim@gmail.com>
2026-01-21 20:43:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 function addToCart(name, price) {
   cart.push({ name, price });
   localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Lisätty ostoskoriin!");
+  alert("Lisätty ostoskoriin");
 }
 
 const list = document.getElementById("cart-items");
@@ -13,10 +13,9 @@ if (list) {
   let sum = 0;
   cart.forEach(item => {
     const li = document.createElement("li");
-    li.textContent = item.name + " – €" + item.price;
+    li.textContent = `${item.name} – €${item.price}`;
     list.appendChild(li);
     sum += item.price;
   });
   total.textContent = "Yhteensä: €" + sum;
 }
-
